7cafa4fee4bd9f0b9e8b19f9b44ad3207c6ee3e2
Ramsey Nasser <ram@nas.sr>
2014-11-08 09:12:12 -0500

    Update data, add data.core

---
+++ data/core.clj
@@ -0,0 +1,42 @@
+(ns data.core
+  (:use arcadia.core)
+  (:require [clojure.edn :as edn]
+            [arcadia.hydrate :as hydrate])
+  (:import [UnityEngine Vector3]))
+
+(defn- vector* [^double x ^double y ^double z]
+  (Vector3. x y z))
+
+(def star-names (future (->> "Assets/Hack/data/star-names.edn"
+                             slurp
+                             edn/read-string
+                             (reduce (fn [acc star] 
+                                       (assoc acc
+                                         (apply vector* ((juxt :x :y :z) star))
+                                         (star :name)))
+                                     {} ))))
+
+(def stars (future (->> "Assets/Hack/data/stars.edn"
+                        slurp
+                        edn/read-string
+                        (map #(hash-map
+                                :position (apply vector* ((juxt :x :y :z) %))
+                                :velocity (apply vector* ((juxt :x :y :z) %))
+                                :speed (% :speed)
+                                :hip (% :hip)))
+                        (map #(assoc %
+                                :name
+                                (or (@star-names (% :position))
+                                    (str "HIP" (% :hip))))))))
+
+(comment (pprint (->> @stars (drop 590)
+                      (take 20)))
+  
+  (doseq [star (take 100 @stars)]
+    (-> (create-primitive :cube)
+        (hydrate/populate! {:transform 
+                            [{:position (star :position)
+                              ; :local-scale (Vector3. 0.1 0.1 0.1)
+                              }]})))
+  
+  (dorun (map destroy (objects-named "Cube"))))
--- data/parse.rb
+++ data/parse.rb
@@ -1,16 +1,38 @@
 require "edn"
 
-file = open("milkyway.spec").read
-lines = file.split("\n")
-data = lines.map { |l| l.strip.split.map { |s| s.to_f } }
-hdata = data.map { |d| { x:d[0],
-                         y:d[1],
-                         z:d[2],
-                         vx:d[9+3],
-                         vy:d[10+3],
-                         vz:d[11+3],
-                         speed:d[12+3],
-                         hip:d[13+3].to_i } }
+def parse_stars
+  file = open("stars.spec").read
+  lines = file.split("\n")
+  data = lines.map { |l| l.strip.split.map { |s| s.to_f } }
+  data.map { |d| { x:d[0],
+                   y:d[1],
+                   z:d[2],
+                   vx:d[9+3],
+                   vy:d[10+3],
+                   vz:d[11+3],
+                   speed:d[12+3],
+                   hip:d[13+3].to_i } }
+end
+
+def parse_star_names
+  file = open("stars.label").read
+  lines = file.split("\n")
+  data = lines.map { |l| l.strip.split }
+  data.map { |d| { x:d[0].to_f,
+                   y:d[1].to_f,
+                   z:d[2].to_f,
+                   name:d[4..-1].join(" ") } }
+end
+
+def parse_exoplanets
+  file = open("expl.label").read
+  lines = file.split("\n")
+  data = lines.map { |l| l.strip.split }
+  data.map { |d| { x:d[0].to_f,
+                   y:d[1].to_f,
+                   z:d[2].to_f,
+                   name:d[4..-1].join(" ") } }
+end
 
 
-p hdata.to_edn
+puts parse_star_names.to_edn
